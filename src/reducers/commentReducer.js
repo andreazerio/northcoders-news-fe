e3bb6872004cf0b86f22b48ac6bb9fa59dda7898
Andrea Zerio <andreazerio@Andreas-MacBook-Pro.local>
2017-11-18 10:43:41 +0000

    adding putComment reducer

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -53,6 +53,29 @@ export default (prevState = initialState, action) => {
         newState.data = [];
         return newState;
     }
+    if (action.type === types.POST_COMMENT_REQUEST) {
+        let newState = Object.assign({}, prevState);
+        newState.loading = true;
+        newState.error = null;
+        newState.data = [];
+        return newState;
+    }
+    if (action.type === types.PUT_COMMENT_SUCCESS) {
+        let newState = Object.assign({}, prevState);
+        const comments = action.payload;
+        newState.loading = false;
+        newState.error = null;
+        newState.data = comments;
+        return newState;
+    }
+    if (action.type === types.PUT_COMMENT_FAILURE) {
+        let newState = Object.assign({}, prevState);
+        const error = action.payload;
+        newState.loading = false;
+        newState.error = error;
+        newState.data = [];
+        return newState;
+    }
 
     return prevState
 };
