684cec1a7194a78b5114d1b731f4522b388fd155
Andrea Zerio <andreazerio@Andreas-MacBook-Pro.local>
2017-11-19 16:17:43 +0000

    testing fetchComments dispatches fetchCommentsSuccess when receiving the correct data with a status code of 200

--- spec/actions.spec.js
+++ spec/actions.spec.js
@@ -1,6 +1,7 @@
 import * as types from '../src/types/index';
 import * as articleActions from '../src/actions/articles';
 import * as commentsActions from '../src/actions/comments'
+import * as topicsActions from '../src/actions/topics'
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import nock from 'nock';
@@ -217,151 +218,176 @@ describe('async actions', () => {
                 });
             });
         });
-    });
-
-    describe('postComment', () => {
-        it('exists', () => {
-            expect(commentsActions.postComment).to.be.a('function');
-        });
-        it('dispatches postCommentsSuccess with the correct article_id as argument when recieving data with status code 200', () => {
-            const article_id = '5a0b3622eccf201ad70df0a4';
-            const comment = {
-                body:'comment test',
-                created_by: 'northcoder'
-            }
-            nock(API_URL)
-            .post(`/articles/${article_id}/comments`, comment)
-            .reply(201, {comment});
-      
-          const expectedActions = [
-            commentsActions.postCommentRequest(article_id, comment),
-            commentsActions.postCommentSuccess(comment)
-          ];
-      
-          const store = mockStore();
+        describe('postComment', () => {
+            it('exists', () => {
+                expect(commentsActions.postComment).to.be.a('function');
+            });
+            it('dispatches postCommentsSuccess with the correct article_id as argument when recieving data with status code 200', () => {
+                const article_id = '5a0b3622eccf201ad70df0a4';
+                const comment = {
+                    body:'comment test',
+                    created_by: 'northcoder'
+                }
+                nock(API_URL)
+                .post(`/articles/${article_id}/comments`, comment)
+                .reply(201, {comment});
           
-          return store.dispatch(commentsActions.postComment(article_id, comment))
-            .then(() => {
-              expect(store.getActions()).to.eql(expectedActions);
+              const expectedActions = [
+                commentsActions.postCommentRequest(article_id, comment),
+                commentsActions.postCommentSuccess(comment)
+              ];
+          
+              const store = mockStore();
+              
+              return store.dispatch(commentsActions.postComment(article_id, comment))
+                .then(() => {
+                  expect(store.getActions()).to.eql(expectedActions);
+                });
+            });
+            it('dispatches postCommentFailure when receiving an error', () => {
+                const comment = {
+                    body:'comment test',
+                    created_by: 'northcoder'
+                }
+                const error = 'article not found';
+                const invalid_article_id = 'andrea';
+                nock(API_URL)
+                  .post(`/articles/${invalid_article_id}/comments`, comment)
+                  .replyWithError({'message': error});
+                
+                const expectedActions = [
+                  commentsActions.postCommentRequest(invalid_article_id, comment),
+                  commentsActions.postCommentFailure(error) 
+                ];
+                
+                const store = mockStore();
+            
+                return store.dispatch(commentsActions.postComment(invalid_article_id, comment))
+                  .then(() => {
+                    expect(store.getActions()).to.eql(expectedActions);
+                  });
             });
         });
-        it('dispatches postCommentFailure when receiving an error', () => {
-            const comment = {
-                body:'comment test',
-                created_by: 'northcoder'
-            }
-            const error = 'article not found';
-            const invalid_article_id = 'andrea';
-            nock(API_URL)
-              .post(`/articles/${invalid_article_id}/comments`, comment)
-              .replyWithError({'message': error});
-            
-            const expectedActions = [
-              commentsActions.postCommentRequest(invalid_article_id, comment),
-              commentsActions.postCommentFailure(error) 
-            ];
-            
-            const store = mockStore();
-        
-            return store.dispatch(commentsActions.postComment(invalid_article_id, comment))
-              .then(() => {
-                expect(store.getActions()).to.eql(expectedActions);
+        describe('putComment', () => {
+            it('exists', () => {
+                expect(commentsActions.putComment).to.be.a('function');
+            });
+            it('dispatches putCommentSuccess with the correct comment_id and vote as arguments when recieving data with status code 200', () => {
+                const comment_id = '5a0b3623eccf201ad70df0cb';
+                const vote = 'up'
+                const data = {
+                    id: comment_id,
+                    votes: 1
+                };
+    
+                nock(API_URL)
+                .put(`/comments/${comment_id}?vote=${vote}`)
+                .reply(200, data);
+    
+                const expectedActions = [
+                    commentsActions.putCommentRequest(comment_id, vote),
+                    commentsActions.putCommentSuccess(data)
+                  ];
+                  const store = mockStore();
+    
+                  return store.dispatch(commentsActions.putComment(comment_id, vote))
+                  .then(() => {
+                    expect(store.getActions()).to.eql(expectedActions);
+                  });
               });
+              it('dispatches putCommentFailure when receiving an error', () => {
+                const vote = 'up';
+                const invalid_comment_id = 'Andrea';
+                const error = {message: 'error - comment_id not valid'}
+              nock(API_URL)
+              .put(`/comments/${invalid_comment_id}?vote=${vote}`)
+                .replyWithError(error.message);
+              
+              const expectedActions = [
+                  commentsActions.putCommentRequest(),
+                  commentsActions.putCommentFailure(error.message)
+              ];
+        
+              const store = mockStore();
+        
+              return store.dispatch(commentsActions.putComment(invalid_comment_id, vote))
+                .then(() => {
+                  expect(store.getActions()).to.eql(expectedActions);
+                });
+            });
+    
+        });
+        describe('deleteComment', () => {
+            it('exists', () => {
+                expect(commentsActions.deleteComment).to.be.a('function');
+            });
+            it('dispatches deleteCommentSuccess action when recieving an action with status code of 200', () => {
+                const comment_id = '5a0b3623eccf201ad70df0cb';
+                nock(API_URL)
+                  .delete(`/comments/${comment_id}`) 
+                  .reply(204, {});
+          
+                const expectedActions = [
+                  commentsActions.deleteCommentRequest(comment_id),
+                  commentsActions.deleteCommentSuccess({})
+                ];
+          
+                const store = mockStore();
+          
+                return store.dispatch(commentsActions.deleteComment(comment_id))
+                  .then(() => {
+                    expect(store.getActions()).to.eql(expectedActions);
+                  });
+            });
+            it('dispatches deleteCommentFailure when receiving an error', () => {
+                const invalid_comment_id = 'Andrea';
+                const error = {message: 'error - comment_id not valid'}
+              nock(API_URL)
+              .delete(`/comments/${invalid_comment_id}`)
+                .replyWithError(error.message);
+              
+              const expectedActions = [
+                  commentsActions.deleteCommentRequest(),
+                  commentsActions.deleteCommentFailure(error.message)
+              ];
+        
+              const store = mockStore();
+        
+              return store.dispatch(commentsActions.deleteComment(invalid_comment_id))
+                .then(() => {
+                  expect(store.getActions()).to.eql(expectedActions);
+                });
+            });
         });
     });
 
-    describe('putComment', () => {
+describe('topics actions', () => {
+    describe('fetchTopics', () => {
         it('exists', () => {
-            expect(commentsActions.putComment).to.be.a('function');
+            expect(topicsActions.fetchTopics).to.be.a('function');
         });
-        it('dispatches putCommentSuccess with the correct comment_id and vote as arguments when recieving data with status code 200', () => {
-            const comment_id = '5a0b3623eccf201ad70df0cb';
-            const vote = 'up'
-            const data = {
-                id: comment_id,
-                votes: 1
-            };
-
+        it('dispatches fetchTopicsSuccess when recieving data with status code 200', () => {
             nock(API_URL)
-            .put(`/comments/${comment_id}?vote=${vote}`)
-            .reply(200, data);
+            .get('/topics')
+            .reply(200, {
+                topics: ['topic1', 'topic2', 'topic3']
+            });
 
             const expectedActions = [
-                commentsActions.putCommentRequest(comment_id, vote),
-                commentsActions.putCommentSuccess(data)
+                topicsActions.fetchTopicsRequest(),
+                topicsActions.fetchTopicsSuccess(['topic1', 'topic2', 'topic3'])
               ];
               const store = mockStore();
 
-              return store.dispatch(commentsActions.putComment(comment_id, vote))
-              .then(() => {
-                expect(store.getActions()).to.eql(expectedActions);
-              });
-          });
-          it('dispatches putCommentFailure when receiving an error', () => {
-            const vote = 'up';
-            const invalid_comment_id = 'Andrea';
-            const error = {message: 'error - comment_id not valid'}
-          nock(API_URL)
-          .put(`/comments/${invalid_comment_id}?vote=${vote}`)
-            .replyWithError(error.message);
-          
-          const expectedActions = [
-              commentsActions.putCommentRequest(),
-              commentsActions.putCommentFailure(error.message)
-          ];
-    
-          const store = mockStore();
-    
-          return store.dispatch(commentsActions.putComment(invalid_comment_id, vote))
-            .then(() => {
-              expect(store.getActions()).to.eql(expectedActions);
-            });
-        });
-
-    });
-
-    describe('deleteComment', () => {
-        it('exists', () => {
-            expect(commentsActions.deleteComment).to.be.a('function');
-        });
-        it('dispatches deleteCommentSuccess action when recieving an action with status code of 200', () => {
-            const comment_id = '5a0b3623eccf201ad70df0cb';
-            nock(API_URL)
-              .delete(`/comments/${comment_id}`) 
-              .reply(204, {});
-      
-            const expectedActions = [
-              commentsActions.deleteCommentRequest(comment_id),
-              commentsActions.deleteCommentSuccess({})
-            ];
-      
-            const store = mockStore();
-      
-            return store.dispatch(commentsActions.deleteComment(comment_id))
+              return store.dispatch(topicsActions.fetchTopics())
               .then(() => {
                 expect(store.getActions()).to.eql(expectedActions);
               });
         });
-        it('dispatches deleteCommentFailure when receiving an error', () => {
-            const invalid_comment_id = 'Andrea';
-            const error = {message: 'error - comment_id not valid'}
-          nock(API_URL)
-          .delete(`/comments/${invalid_comment_id}`)
-            .replyWithError(error.message);
-          
-          const expectedActions = [
-              commentsActions.deleteCommentRequest(),
-              commentsActions.deleteCommentFailure(error.message)
-          ];
-    
-          const store = mockStore();
-    
-          return store.dispatch(commentsActions.deleteComment(invalid_comment_id))
-            .then(() => {
-              expect(store.getActions()).to.eql(expectedActions);
-            });
-        });
     });
+});
+
+
 });
 
 
